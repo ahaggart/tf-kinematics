f9300106d6bac059edac1abf9f96cf33ca96bfe1
Alex <ahaggart@ucsd.edu>
2018-02-10 14:51:47 -0800

    seems like it works

--- kinematics.py
+++ kinematics.py
@@ -2,46 +2,79 @@
 
 import numpy as np
 import tensorflow as tf
+import random
 
-def input_fn(features, labels, batch_size):
-    """An input function for training"""
-    # Convert the inputs to a Dataset.
-    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+def kin_input_gen(kin_fn,batch_size,num_joints):
+    iterations = range(0,batch_size)
+    thetas = []
+    lengths = []
+    for joint in range(0,num_joints):
+        thetas.append(tf.constant([[random.random()*2*np.pi] for i in iterations],dtype=np.float32))
+        lengths.append(tf.constant([[random.random()] for i in iterations],dtype=np.float32))
+    theta_features = dict([('theta{}'.format(i),thetas[i]) for i in range(0,num_joints)])
+    length_features = dict([('len{}'.format(i),lengths[i]) for i in range(0,num_joints)])
 
-    # Shuffle, repeat, and batch the examples.
-    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
+    positions = kin_fn(tf.concat(thetas,1),tf.concat(lengths,1)) 
+    perturbation_amount = 0.01 #centimeters(?)
+    perturbation_t = tf.constant([[perturbation_amount]])
+    perturbations = tf.concat([tf.multiply(
+                        tf.constant([[random.random(),random.random(),random.random()]]),
+                        perturbation_t) 
+                    for i in iterations],0)
+    goal_positions_t = tf.add(positions,perturbations)
+    goal_positions = {
+        "goal_x":goal_positions_t[:,0],
+        "goal_y":goal_positions_t[:,1],
+        "goal_z":goal_positions_t[:,2]
+    }
+    # merge all the dictionaries
+    features = {}
+    features.update(theta_features)
+    features.update(length_features)
+    features.update(goal_positions)
+    return (features,goal_positions_t)
 
-    # Return the read end of the pipeline.
-    return dataset.make_one_shot_iterator().get_next()
+def move_to_position(x,y,z):
+    goal_v = np.array([x,y,z])
+    goal = tf.constant(goal_v,dtype=tf.float32)
+    segments = tf.constant([[1,1,1]],dtype=tf.float32)
+    start_angles = tf.constant([[0,0,0]],dtype=tf.float32)
+    start_pos = forward_kinematics_3(
+        start_angles,
+        segments
+    )
 
-
-
-seg_zero_len = tf.feature_column.numeric_column(key="len0")
-seg_one_len  = tf.feature_column.numeric_column(key="len1")
-seg_two_len  = tf.feature_column.numeric_column(key="len2")
-
-joint_zero_theta = tf.feature_column.numeric_column(key="theta0")
-joint_one_theta  = tf.feature_column.numeric_column(key="theta1")
-joint_two_theta  = tf.feature_column.numeric_column(key="theta2")
-
-goal_x = tf.feature_column.numeric_column(key="goal_x")
-goal_y = tf.feature_column.numeric_column(key="goal_y")
-goal_z = tf.feature_column.numeric_column(key="goal_z")
-
-feature_columns = [seg_zero_len,seg_one_len,seg_two_len,
-                   joint_zero_theta,joint_one_theta,joint_two_theta,
-                   goal_x,goal_y,goal_z]
+    path = np.subtract(goal_v,np.array([1,0,0]))
+    path_len = np.linalg.norm(path)
+    step_size = 0.01
+    checkpoints = [
+        tf.add(start_pos,tf.multiply(tf.constant([[step]]),path))
+        for step in np.arange(0,path_len,step_size)
+    ]
+    yield {
+        "goal_x":checkpoints[1][:,0],
+        "goal_y":checkpoints[1][:,1],
+        "goal_z":checkpoints[1][:,2],
+        "theta0":[[0.0]],
+        "theta1":[[0.0]],
+        "theta2":[[0.0]],
+        "len0":[[1.0]],
+        "len1":[[1.0]],
+        "len2":[[1.0]]
+    }
 
 # transform a tensor from joint-space to cartesian space
 def forward_kinematics_3(thetas,lengths):
-    sins = tf.sin(thetas)
-    coss = tf.cos(thetas)
-    x_comps = tf.mul(lengths,coss)
-    y_comps = tf.mul(lengths,sins)
-    x_final = tf.reduce_sum(x_comps)
-    y_final = tf.reduce_sum(y_comps)
-    z_final = tf.constant([0])
-    return tf.concat([x_final,y_final,z_final],0)
+    sins = tf.sin(tf.cumsum(thetas,1))
+    coss = tf.cos(tf.cumsum(thetas,1))
+    x_comps = tf.multiply(lengths,coss) # could use tensordot
+    y_comps = tf.multiply(lengths,sins)
+
+    x_final = tf.reduce_sum(x_comps,1,keepdims=True)
+    y_final = tf.reduce_sum(y_comps,1,keepdims=True)
+    z_final = tf.zeros(tf.shape(x_final),dtype=tf.float32)
+
+    return tf.concat([x_final,y_final,z_final],1)
 
 
 def kin_solver_fn(features,labels,mode,params):
@@ -58,11 +91,11 @@ def kin_solver_fn(features,labels,mode,params):
     result = tf.layers.dense(net,units=params['num_joints'],activation=None)
 
     # build a 2 dimensional tensor by concatenating length tensors
-    length_list = [features['len{}'].format(i) for i in range(0,params['num_joints'])]
+    length_list = [features['len{}'.format(i)] for i in range(0,params['num_joints'])]
     lengths = tf.concat(length_list,1)
 
     # build a 2 dimensional tensor by concatenating angle tensors
-    theta_list = [features['theta{}'].format(i) for i in range(0,params['num_joints'])]
+    theta_list = [features['theta{}'.format(i)] for i in range(0,params['num_joints'])]
     start_angles = tf.concat(length_list,1)
 
     # use the supplied kinematic equation to find end affector location
@@ -70,33 +103,99 @@ def kin_solver_fn(features,labels,mode,params):
     kinematics = params['kinematic_eqn'](result,lengths)
 
     #actions for prediction mode
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        predictions = {kinematics:result}
+        return tf.estimator.EstimatorSpec(mode,predictions)
 
     # define the loss layers
     # compute the distance from the desired result
-    distance_loss = tf.losses.mean_squared_error(
-        labels=labels,
-        predictions=kinematics,
-        reduction=None)
+    distance_loss = tf.reduce_mean(
+                        tf.sqrt(
+                            tf.reduce_sum(
+                                tf.square(tf.subtract(kinematics,labels)),
+                                [1])),
+                        [0]
+                    )
 
     # compute the difference from the calculated state to the previous state
     # minimize joint adjustment to reach new position
     angular_diff = tf.subtract(result,start_angles)
-    angular_loss = tf.atan(tf.sin(angular_diff),tf.cos(angular_diff))
+    angular_loss = tf.reduce_sum(tf.atan2(tf.sin(angular_diff),tf.cos(angular_diff)),1)
 
     # TODO: how to weight the angular and distance losses?
-
-    #actions for training mode
+    combined_loss = distance_loss
 
     #actions for eval mode
-	
-    pass
+    if mode == tf.estimator.ModeKeys.EVAL:
+        return tf.estimator.EstimatorSpec(
+                mode,
+                loss=combined_loss,
+                eval_metric_ops={"angular_loss":angular_loss,
+                                 "distance_loss":distance_loss}
+                )
 
-num_joints = 3
-kinematics_solver = tf.estimator.Estimator(
-    model_fn=kin_solver_fn,
-    model_dir="~/Documents/Yonder/kiNNematics/models/{}seg".format(num_joints),
-    params={
-        'feature_columns':feature_columns,
-        'num_joints':num_joints,
-        'kinematic_eqn':forward_kinematics_3})
+    #actions for train mode
+    assert mode == tf.estimator.ModeKeys.TRAIN 
+
+    optimizer = tf.train.AdamOptimizer()
+    train_op = optimizer.minimize(combined_loss, global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode, loss=combined_loss, train_op=train_op)
+
+
+# test kinematic functions
+# test_theta  = tf.constant([[np.pi/2,-np.pi/2,-np.pi/2]],dtype=tf.float32)
+# test_length = tf.constant([[1,1,1]],dtype=tf.float32)
+# test_kin    = forward_kinematics_3(test_theta,test_length)
+
+# with tf.Session() as sess:
+#     print(sess.run(test_kin))
+
+def main(argv):
+    num_joints = 3
+    kinematic_eqn = forward_kinematics_3
+    batch_size=1000
+    hidden_layers=10
+    hidden_layer_size=15
+
+    seg_zero_len = tf.feature_column.numeric_column(key="len0")
+    seg_one_len  = tf.feature_column.numeric_column(key="len1")
+    seg_two_len  = tf.feature_column.numeric_column(key="len2")
+
+    joint_zero_theta = tf.feature_column.numeric_column(key="theta0")
+    joint_one_theta  = tf.feature_column.numeric_column(key="theta1")
+    joint_two_theta  = tf.feature_column.numeric_column(key="theta2")
+
+    goal_x = tf.feature_column.numeric_column(key="goal_x")
+    goal_y = tf.feature_column.numeric_column(key="goal_y")
+    goal_z = tf.feature_column.numeric_column(key="goal_z")
+
+    feature_columns = [seg_zero_len,seg_one_len,seg_two_len,
+                       joint_zero_theta,joint_one_theta,joint_two_theta,
+                       goal_x,goal_y,goal_z]
+
+    model_dir_base = "models/{}seg/{}l{}u";
+
+    kinematics_solver = tf.estimator.Estimator(
+        model_fn=kin_solver_fn,
+        model_dir=model_dir_base.format(num_joints,hidden_layers,hidden_layer_size),
+        params={
+            'feature_columns':feature_columns,
+            'num_joints':num_joints,
+            'kinematic_eqn':kinematic_eqn,
+            'hidden_units':[hidden_layer_size for i in range(0,hidden_layers)]
+            })
+
+    if argv[1] == '--train':
+        input_fn = lambda:kin_input_gen(kinematic_eqn,batch_size,num_joints)
+        kinematics_solver.train(input_fn=input_fn,steps=100000)
+    elif argv[1] == '--solve':
+        solutions = kinematics_solver.predict(
+            input_fn=lambda:move_to_position(float(argv[2]),float(argv[2]),float(argv[3]))
+        )
+        for solution in solutions:
+            print(solution)
+
+if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run(main)
 
